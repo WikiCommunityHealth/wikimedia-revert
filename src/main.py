bd9b133c48a2f22a56c336566666a7a9a18c4b2e
alessiogandelli <alessiogandelli99@gmail.com>
2021-03-13 17:43:05 +0100

    format

--- src/main.py
+++ src/main.py
@@ -11,17 +11,15 @@ dataset = '/home/gandelli/dev/data/it/filtered_sorted_it.tsv.bz2'
 output = '/home/gandelli/dev/data/wars/'
 
 
-#l'ultima colonna è fals invece che false 
+# l'ultima colonna è fals invece che false
 
 
-
-#%% functions 
+# %% functions
 
 def complex_chains():
-    
 
-    #dump_in = bz2.open(dataset, 'r') 
-    dump_in = open(dataset, 'r')# for uncompressed 
+    #dump_in = bz2.open(dataset, 'r')
+    dump_in = open(dataset, 'r')  # for uncompressed
     dump_out = open('wars.json', 'w')
     line = dump_in.readline()
 
@@ -47,16 +45,14 @@ def complex_chains():
     }
 
     while line != '':
-        
-        #line = dump_in.readline().rstrip().decode('utf-8')[:-1] 
-        line = dump_in.readline().rstrip()[:-1]# for uncompressed 
+
+        #line = dump_in.readline().rstrip().decode('utf-8')[:-1]
+        line = dump_in.readline().rstrip()[:-1]  # for uncompressed
         values = line.split('\t')
 
         if line == '' or len(values) < 69 or values[28] != '0':
             continue
-        
-        
- 
+
         page_name = values[25]
         rev_id = values[52]
         reverter = values[65]
@@ -68,54 +64,55 @@ def complex_chains():
         added = False
         exist = False
 
-        #check if this revision is part of a chain
+        # check if this revision is part of a chain
         if page_name == page:
-            for chain in open_chains:                                   #check if this rev is part of an existing  chain
+            for chain in open_chains:  # check if this rev is part of an existing  chain
 
-                for i in range(len(chain)):                             # if the one you want to insert already exists don't add it 
+                # if the one you want to insert already exists don't add it
+                for i in range(len(chain)):
                     if chain[i] == reverter:
                         exist = True
 
-                if chain[-1] == rev_id:                                 # if the last element of the chain match with the current revision id
+                # if the last element of the chain match with the current revision id
+                if chain[-1] == rev_id:
                     added = True
                     if is_reverted == 'true' and not exist:                           # continue the chain
                         chain.append(reverter)
                         utenti.append(utente)
-                         
-                    else:                                               # end of the chain this revision is not reverted 
-                        if len(chain) > 2 :
+
+                    else:                                               # end of the chain this revision is not reverted
+                        if len(chain) > 2:
                             catena['revisions'] = chain
                             catena['users'] = utenti
                             war['chains'].append(catena)
                             complete_chains.append(chain)
-                        open_chains.remove(chain)                       
-           
+                        open_chains.remove(chain)
 
             if not added and not exist:
-                
+
                 open_chains.append([rev_id, reverter])
-        
+
         else:
             if(len(complete_chains) > 0):
                 page_chains[page] = complete_chains
                 war['page'] = page
-                
+
                 dump_out.write(json.dumps(war))
                 dump_out.write(',\n')
             page = page_name
             complete_chains = []
             open_chains = []
-    
+
     dump_out.write(']')
     return page_chains
 
+
 def simple_chains():
 
-    
     dump_in = bz2.open(dataset, 'r')
-    #dump_in = open(dataset, 'r')# for uncompressed 
+    # dump_in = open(dataset, 'r')# for uncompressed
     line = dump_in.readline()
-    
+
     i = 0
 
     reverter_id = 0
@@ -127,64 +124,67 @@ def simple_chains():
     users = set()
     total_reverts = 0
     longest_chain = 0
-    
+
     while line != '':
 
         line = dump_in.readline().rstrip().decode('utf-8')[:-1]
-        #line = dump_in.readline().rstrip()[:-1]# for uncompressed 
+        # line = dump_in.readline().rstrip()[:-1]# for uncompressed
         values = line.split('\t')
 
-        if line == '' or values[28] != '0' or is_vandalism(values[4]): # i want only namespace 0 and no vandalism
+        # i want only namespace 0 and no vandalism
+        if line == '' or values[28] != '0' or is_vandalism(values[4]):
             continue
 
-        
-        
-        page_name   = values[25]
-        rev_id      = values[52]
-        reverter    = values[65]
+        page_name = values[25]
+        rev_id = values[52]
+        reverter = values[65]
         is_reverted = values[64]
-        user        = values[7]
-        page_id     = int(values[23])
+        user = values[7]
+        page_id = int(values[23])
 
-        if page_name != current_page:                       #process new page 
-            
+        if page_name != current_page:  # process new page
+
             if(len(chains) > 0):
-                savePage(current_page, chains, page_id, total_reverts/len(chains), longest_chain)
+                savePage(current_page, chains, page_id,
+                         total_reverts/len(chains), longest_chain)
                 page_chains[current_page] = chains
-                stats[current_page] = (total_reverts/len(chains) , longest_chain)
+                stats[current_page] = (
+                    total_reverts/len(chains), longest_chain)
                 #print(current_page, total_reverts/len(chains), longest_chain)
-                i+=1
+                i += 1
 
-            current_page = page_name    
-            chains = [] 
+            current_page = page_name
+            chains = []
             total_reverts = 0
             longest_chain = 0
-             
+
         else:
-            if rev_id == reverter_id:                                   #if the currect reverts the previous one
-                chain.append(rev_id)     
-                users.add(user)                               # continue the chain
+            if rev_id == reverter_id:  # if the currect reverts the previous one
+                chain.append(rev_id)
+                # continue the chain
+                users.add(user)
             else:
                 if len(chain) > 2:
-                    chains.append({'revisions':chain, 'users' : list(users)})
+                    chains.append({'revisions': chain, 'users': list(users)})
                     total_reverts += len(chain)
                     longest_chain = max(longest_chain, len(chain))
                 chain = [rev_id]
                 users = {user}
-                
 
             if is_reverted == 'true':
-                reverter_id = reverter # save
-            
+                reverter_id = reverter  # save
+
     finish_files()
-    return (page_chains,stats)
+    return (page_chains, stats)
+
 
 def is_vandalism(comment):
     words = re.compile('vandal')
-    if words.search(comment): 
+    if words.search(comment):
         return True
     else:
         return False
+
 
 def savePage(title, chains, id, weight, longest):
     path = f"{output}wars_{id%4}.json"
@@ -194,8 +194,8 @@ def savePage(title, chains, id, weight, longest):
     if filesize == 0:
         dump_out.write('[')
 
-    
-    dump_out.write(json.dumps({'title': title, 'chains': chains, 'mean': weight, 'longest': longest})+',')
+    dump_out.write(json.dumps(
+        {'title': title, 'chains': chains, 'mean': weight, 'longest': longest})+',')
     dump_out.close()
 
 
@@ -203,17 +203,18 @@ def finish_files():
     for filename in os.listdir(output):
         print(filename)
         dump_out = open(output+filename, 'a')
-        dump_out.write('{}]') # andrebbe cancellata la virgola, uso questo trick per farlo sintatticamente corretto
+        # andrebbe cancellata la virgola, uso questo trick per farlo sintatticamente corretto
+        dump_out.write('{}]')
+
 
 def get_DataFrame():
 
     dump_in = bz2.open(dataset, 'r')
     line = dump_in.readline()
-    df =[]
-    
+    df = []
 
     while line != '':
- 
+
         line = dump_in.readline().rstrip().decode('utf-8')[:-1]
         values = line.split('\t')
 
@@ -222,36 +223,33 @@ def get_DataFrame():
     return pd.DataFrame(df)
 
 
-#%%
+# %%
 
 
-
-
-# %% COMPLEX 
+# %% COMPLEX
 inizio = datetime.now()
 c_chains = complex_chains()
 sorted(c_chains, key=lambda k: len(c_chains[k]), reverse=True)
-print(datetime.now() -inizio)
-
-
+print(datetime.now() - inizio)
 
 
 # %% SIMPLE
 inizio = datetime.now()
 s_chains, stats = simple_chains()
 sorted(s_chains, key=lambda k: len(s_chains[k]), reverse=True)
-sorted(stats.items(), key=lambda k: k[1][1], reverse = True) # catena piu lunga
-sorted(stats.items(), key=lambda k: k[1][0], reverse = True) # media
-print(datetime.now() -inizio)
+sorted(stats.items(), key=lambda k: k[1][1], reverse=True)  # catena piu lunga
+sorted(stats.items(), key=lambda k: k[1][0], reverse=True)  # media
+print(datetime.now() - inizio)
 
 
+# %% sort pages by revert number
 
-#%% sort pages by revert number
-
-df = get_DataFrame()    
-df = df.filter([3,4,7,25,34,64,67], axis = 1) 
-df.columns = ['timestamp','commento', 'utente','pagina' , 'edit_count','reverted', 'reverta' ]
-df_by_page = df.groupby(['pagina']).count().sort_values(by=['utente'], ascending=False)
+df = get_DataFrame()
+df = df.filter([3, 4, 7, 25, 34, 64, 67], axis=1)
+df.columns = ['timestamp', 'commento', 'utente',
+              'pagina', 'edit_count', 'reverted', 'reverta']
+df_by_page = df.groupby(['pagina']).count().sort_values(
+    by=['utente'], ascending=False)
 
 
 # %%
@@ -259,15 +257,7 @@ df_by_page = df.groupby(['pagina']).count().sort_values(by=['utente'], ascending
 # %%
 
 
-
-
-
-
-
-
-
-
-#%% non so perchè ma non funziona o  forse noon funziona l'altro 
+# %% non so perchè ma non funziona o  forse noon funziona l'altro
 
 
 # %%
--- src/sort_dataset.py
+++ src/sort_dataset.py
@@ -6,19 +6,20 @@ from datetime import datetime
 
 inizio = datetime.now()
 
-dataset_folder = '/home/gandelli/dev/data/it/';
-output_file = '/home/gandelli/dev/data/revisions.tsv';
+dataset_folder = '/home/gandelli/dev/data/it/'
+output_file = '/home/gandelli/dev/data/revisions.tsv'
 
-sort_script = '/home/gandelli/dev/wikimedia-revert/src/bash/sort.sh';
+sort_script = '/home/gandelli/dev/wikimedia-revert/src/bash/sort.sh'
 
 output = open(output_file, "w")
 
 for year in range(2001, 2021):
 
-    dump_in = bz2.open(dataset_folder+'/it'+ str(year) +'.tsv.bz2', 'r')
+    dump_in = bz2.open(dataset_folder+'/it' + str(year) + '.tsv.bz2', 'r')
     line = dump_in.readline()
 
-    print('ora inizio a togliere tutto tranne revert e revertati dell anno'+ str(year)+ "  ", datetime.now()-inizio)
+    print('ora inizio a togliere tutto tranne revert e revertati dell anno' +
+          str(year) + "  ", datetime.now()-inizio)
     while line != '':
         line = dump_in.readline().rstrip().decode('utf-8')[:-1]
         values = line.split('\t')
@@ -27,14 +28,12 @@ for year in range(2001, 2021):
 
         if values[1] != 'revision':
             continue
-        
-        if values[64] == 'false' and values[67] == 'false': 
-            continue 
 
-        
+        if values[64] == 'false' and values[67] == 'false':
+            continue
+
         output.write(line + '\n')
     dump_in.close()
-
 
 
 output.close()
@@ -42,6 +41,4 @@ print('finito di filtrare ora inizio a sortare ', datetime.now()-inizio)
 subprocess.call(sort_script)
 print('sorted in ', datetime.now()-inizio)
 
-#os.remove(output_file)
-
-
+# os.remove(output_file)
